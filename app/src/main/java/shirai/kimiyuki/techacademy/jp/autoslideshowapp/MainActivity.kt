be36ccad945e28c51502dfa83417af05ff62d923
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-11 17:46:38 +0900

    権限否認の場合の処理を追加

--- app/src/main/java/shirai/kimiyuki/techacademy/jp/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/jp/autoslideshowapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package shirai.kimiyuki.techacademy.jp.autoslideshowapp
 
 import android.content.pm.PackageManager
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.support.design.widget.Snackbar
 import android.os.Bundle
@@ -10,8 +9,12 @@ import android.Manifest
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.content.DialogInterface
 import android.database.Cursor
+import android.os.Build
+import android.support.v7.app.AlertDialog
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -19,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
     private var cursor: Cursor? = null
-    private var isPlaying = false
     private var mTimer: Timer? = null
     private val mHandler = Handler()
     private var myCallback: () -> Unit = {}
@@ -27,42 +29,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myCallback = {
-            setupCursor()
-            setImageToImageView()
-        }
-        my_request_permission()
+        myRequestPermission()
         Log.d("hello", "End OnCreate")
         //end of flow on Create
 
         //set listeners
-        backward_button.setOnClickListener {
-            myCallback = { back_image(it) }
-            my_request_permission()
-        }
+        backward_button.setOnClickListener { backImage(it) }
 
-        forward_button.setOnClickListener {
-            myCallback = { forward_image(it) }
-            my_request_permission()
-        }
+        forward_button.setOnClickListener { forwardImage(it) }
 
-        play_stop_button.setOnClickListener {
-            myCallback = { play_stop(it) }
-            my_request_permission()
-        }
+        play_stop_button.setOnClickListener { playStop(it) }
 
     }
 
-    private fun my_request_permission() {
+    private fun myRequestPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val ret = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
             if (ret != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
             } else {
-                myCallback()
+                setupCursor()
             }
         } else {
-            myCallback()
+            setupCursor()
         }
     }
 
@@ -70,45 +59,49 @@ class MainActivity : AppCompatActivity() {
         Log.d("hello", "onReqPermission:${requestCode}")
         if (requestCode != PERMISSIONS_REQUEST_CODE) return
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            myCallback()
+            setupCursor()
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setNeutralButton("終了します") { dialog, which ->
+                if (which == DialogInterface.BUTTON_NEUTRAL) {
+                    finish()
+                }
+            }
+            builder.setTitle("権限がない場合は終了します").show()
         }
     }
 
-    private fun back_image(v: View) {
-        if(cursor == null) setupCursor()
-        Log.d("hello", "start back_image")
-        if (isPlaying) {
-            val snack = Snackbar.make(v, "再生中です。停止してからです", 3000)
-            snack.setAction("了解") { /*no response*/ }.show()
+    private fun backImage(v: View) {
+        Log.d("hello", "start backImage")
+        if (mTimer != null) {
+            showSnack(v, "再生中です。停止を押してからです", 3000)
             return
         }
         if (cursor!!.isFirst) cursor?.moveToLast() else cursor?.moveToPrevious()
         setImageToImageView()
     }
 
-    private fun forward_image(v: View) {
+    private fun forwardImage(v: View) {
         Log.d("hello", "start forward_image")
-        if(cursor == null) setupCursor()
-        if (isPlaying) {
-            val snack = Snackbar.make(v, "再生中です。停止を押してからです", 3000)
-            snack.setAction("了解") {}
-            snack.show()
-            Log.d("hello", "isplaying")
-            //it.animate().rotation(180.0f).alpha(1.0f).setDuration(300).start()
+        if (mTimer != null) {
+            showSnack(v, "再生中です。停止を押してからです", 3000)
             return
         }
         if (cursor!!.isLast) cursor?.moveToFirst() else cursor?.moveToNext()
         setImageToImageView()
     }
 
-    private fun play_stop(v: View) {
-        if(cursor == null) setupCursor()
-        play_stop_button.text = if (isPlaying) "再生" else "停止"
-        isPlaying = !isPlaying
-        if (isPlaying) {
-            if (mTimer != null) {
-                return
-            }
+    private fun showSnack(v: View, text: String, duration: Int) {
+        val snack: Snackbar = Snackbar.make(v, text, duration)
+        snack.setAction("了解") {}
+        snack.show()
+    }
+
+    private fun playStop(v: View) {
+        if (mTimer == null) {
+            play_stop_button.text = "停止"
+            backward_button.isEnabled = false
+            forward_button.isEnabled = false
             Snackbar.make(v, "再生します", 1000).show()
             mTimer = Timer()
             mTimer!!.schedule(
@@ -123,11 +116,12 @@ class MainActivity : AppCompatActivity() {
                 2000, 2000
             )
         } else {
-            if (mTimer != null) {
-                Snackbar.make(v, "停止します", 1000).show()
-                mTimer!!.cancel()
-                mTimer = null
-            }
+            play_stop_button.text = "停止"
+            backward_button.isEnabled = true
+            forward_button.isEnabled = true
+            Snackbar.make(v, "停止します", 1000).show()
+            mTimer!!.cancel()
+            mTimer = null
         }
     }
 
@@ -147,6 +141,7 @@ class MainActivity : AppCompatActivity() {
             null // ソート (null ソートなし)
         )
         cursor?.moveToFirst()
+        setImageToImageView()
         Log.d("hello", "setupCursor:${cursor?.position}")
     }
 
@@ -159,7 +154,7 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageURI(imageUri)
             Log.d("hello setImage", "URI : " + imageUri.toString())
         } else {
-            //TODO no image
+            Toast.makeText(this, "画像がありません", Toast.LENGTH_LONG)
         }
     }
 }
